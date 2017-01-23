34de519f7f179a69a00b771c7596828e3c5699ea
kthornt2 <kthornt2@oakland.edu>
2017-01-22 20:46:44 -0500

    Initial commit

---
+++ app/src/main/java/capstone/textblock/textblock/get_coordinates.java
@@ -0,0 +1,47 @@
+package capstone.textblock.textblock;
+import android.location.Location;
+
+
+public class get_coordinates extends Location {
+
+
+
+    public get_coordinates(Location location) {
+        // TODO Auto-generated constructor stub
+        super(location);
+
+    }
+
+
+
+    @Override
+    public float distanceTo(Location dest) {
+        // TODO Auto-generated method stub
+        float nDistance = super.distanceTo(dest);
+        return nDistance;
+    }
+
+    @Override
+    public float getAccuracy() {
+        // TODO Auto-generated method stub
+        float nAccuracy = super.getAccuracy();
+        return nAccuracy;
+    }
+
+    @Override
+    public double getAltitude() {
+        // TODO Auto-generated method stub
+        double nAltitude = super.getAltitude();
+        return nAltitude;
+    }
+
+    @Override
+    public float getSpeed() {
+        // TODO Auto-generated method stub
+        float nSpeed = super.getSpeed() * 3.6f;
+        return nSpeed;
+    }
+
+
+
+}
