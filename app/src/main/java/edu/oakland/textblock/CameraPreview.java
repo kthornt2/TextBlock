05de3d7f2bbed94e5404f79fdac6a33af43f7ae2
toto <toto6321@qq.com>
2017-02-07 20:19:28 -0500

    now I could open a camera with the Camera Class.

--- app/src/main/java/edu/oakland/textblock/CameraPreview.java
+++ app/src/main/java/edu/oakland/textblock/CameraPreview.java
@@ -1,8 +1,10 @@
 package edu.oakland.textblock;
 
+import android.app.Activity;
 import android.content.Context;
 import android.hardware.Camera;
 import android.util.Log;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -27,6 +29,38 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
         //deprecated setting, but required on Android Version prior to Android 3.0
         surfaceHolder.setType(SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    private static void setCameraDisplayOrientation(Activity activity, int cameraId, Camera camera) {
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;
+        } else {
+            result = (info.orientation - degrees + 360) % 360;
+        }
+
+        camera.setDisplayOrientation(result);
+
     }
 
     @Override
@@ -62,9 +96,32 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         // set preview size and make any resize, rotate or
         // reformatting changes here
 
+        setCameraDisplayOrientation((Activity) getContext(), Camera.CameraInfo.CAMERA_FACING_BACK, camera);
+        Camera.Parameters parameters = camera.getParameters();
+        Camera.Size bestSize = null;
+        for (Camera.Size size : parameters.getSupportedPictureSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (bestSize == null) {
+                    bestSize = size;
+                } else {
+                    int bestArea = bestSize.width * bestSize.height;
+                    int newArea = size.width * size.height;
+
+                    if (newArea > bestArea) {
+                        bestSize = size;
+                    }
+                }
+            }
+        }
+
+        parameters.setPictureSize(bestSize.width, bestSize.height);
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+        camera.setParameters(parameters);
+
+
         // start preview with new settings
         try {
-            camera.setPreviewDisplay(surfaceHolder);
+            camera.setPreviewDisplay(holder);
             camera.startPreview();
         } catch (Exception e) {
             Log.d(TAG, "Error starting camera preview: " + e.getMessage());
