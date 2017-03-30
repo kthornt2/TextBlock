3f6bfd087ef33cd4b071b78c77073ae0f04b47b6
kthornt2 <kthornt2@oakland.edu>
2017-03-29 22:55:14 -0400

    Add files via upload
    Updated and added code to GPS to fix realtime tracking and implement Broadcast listener.  Added broadcast reciever to Block Screen.  Fixed lock statement.

---
+++ app/src/main/java/edu/oakland/textblock/BootReceiver2.java
@@ -0,0 +1,21 @@
+package edu.oakland.textblock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Remzorz on 3/10/2017.
+ */
+//creating a broadcast reciever for the gps so the locking mechanism can be worked on later
+public class BootReceiver2 extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context2, Intent intent2) {
+        Intent myIntent = new Intent(context2, GpsServices.class);
+        context2.startService(myIntent);
+        Log.i("Autostart","started");
+    }
+
+}
